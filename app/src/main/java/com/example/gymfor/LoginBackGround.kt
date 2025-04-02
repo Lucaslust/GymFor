51a991a33fe7b5407f6fe4d3b30b7a403c14a0f6
JpABrasil <joaopedroalexandrino6@hotmail.com>
2025-04-02 13:06:54 -0300

    Primeiras Estruturas

--- app/src/main/java/com/example/gymfor/LoginBackGround.kt
+++ app/src/main/java/com/example/gymfor/LoginBackGround.kt
@@ -1,5 +1,7 @@
 package com.example.gymfor
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
@@ -12,13 +14,11 @@ class LoginBackGround: AppCompatActivity() {
         // Define o layout que será exibido
         setContentView(R.layout.login_background)
 
-        // Encontra o TextView pelo ID
-        val textView: TextView = findViewById(R.id.textView)
+        val overlayView = LayoutInflater.from(this).inflate(R.layout.login, null)
+        addContentView(overlayView, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        ))
 
-        // Carrega a fonte personalizada
-        val typeface = ResourcesCompat.getFont(this, R.font.satoshi_regular)
-
-        // Aplica a fonte no TextView
-        textView.typeface = typeface
     }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Estrutura das Telas
+* Background -> Aproveitar Partes Semelhantes (FrameLayout)
+  * Ex: login_background -> BackGround referente a tela de login
+* Partes Intermediárias -> Partes Diferentes(Constraint ou Relative Layout)
+  * Ex: login_camposIniciais -> Campos referente a tela de login
+* Identificadores:
+1. login: Tela de Entrada no aplicativo, desde cadastro para alunos e treinadores até autorização e entrada na conta
